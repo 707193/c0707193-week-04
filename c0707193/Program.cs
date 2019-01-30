b92b674edce484e56e268a0d0b547ef70be3da9f
visualstudio <visualstudio@122-PC06>
2019-01-30 11:20:03 -0500

    i added the country side class and launch method to innitilize the class

--- c0707193/Program.cs
+++ c0707193/Program.cs
@@ -12,4 +12,36 @@ namespace c0707193
         {
         }
     }
+
+    class Village
+    {
+        // node is an ADT
+        // what kind of data do we need in a NODE?
+        public Village nextvillage;
+        public String VillageName;
+        public bool isAstrildeHere = false;
+
+    }
+    class Countryside
+    {
+
+        Village Toronto = new Village();
+        Village Maple = new Village();
+        Village Ajex = new Village();
+        
+
+
+            public void Launch()
+        {
+            Toronto.VillageName = "Toronto";
+            Toronto.nextVillage = Maple;
+            Toronto.previousVillage = null;
+            Maple.VillageName = "Maple";
+            Maple.nextVillage = Ajex;
+            Maple.previousVillage = Toronto;
+            Ajex.VillageName = "Ajex";
+            Ajex.nextVillage = null;
+            Ajex.previousVillage = Maple;
+        }
+    }
 }
